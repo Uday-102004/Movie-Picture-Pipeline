2e3fba94f7ea4b3845c15782cfe9d7f77d85af59
Pentapalli Uday Kumar <udaykumar102004@gmail.com>
2026-08-22 06:27:55 +0000

    Fix movie frontend

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -1,20 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
-function MovieDetail({ movie }) {
+function MovieDetails({ movie }) {
   const [details, setDetails] = useState(null);
+
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`).then((response) => {
-      setDetails(response.data);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`)
+      .then((response) => {
+        setDetails(response.data.movie);
+      })
+      .catch((error) => {
+        console.error('Error fetching movie details:', error);
+      });
   }, [movie]);
 
+  if (!details) {
+    return <p>Loading movie details...</p>;
+  }
+
   return (
-    <div>
-      <h2>{details?.movie.title}</h2>
-      <p>{details?.movie.description}</p>
+    <div className="movieDetails">
+      <h2>{details.title}</h2>
+      <p>{details.description}</p>
     </div>
   );
 }
 
-export default MovieDetail;
+MovieDetails.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  }).isRequired,
+};
+
+export default MovieDetails;
--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,9 +6,14 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch((error) => {
+        console.error('Error fetching movies:', error);
+      });
   }, []);
 
   return (
